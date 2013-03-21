e21b3d6460f3efdc43cb9f70625940c05312864f
Hao Nguyen <hao_nguyen1412@yahoo.com.vn>
2013-03-21 15:26:06 -0700

    Push all the code

---
+++ CodeDay/LoginWrapper.py
@@ -0,0 +1,11 @@
+#Hao Nguyen, John Yeung, Colin Robinson
+import Login
+
+loginInfo = Login.GetLoginPage()
+
+f = open('buffer.txt', 'w')
+f.write(loginInfo[1])
+f.close()
+
+#print out the loginPage
+print loginInfo[0]
